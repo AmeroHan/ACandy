361e3713a2a3cac2d18810dacd23e726440f2e48
AmeroHan <72120616+AmeroHan@users.noreply.github.com>
2024-11-17 10:20:02 +0800

    feat: HTML comment

--- classes.lua
+++ classes.lua
@@ -4,6 +4,37 @@ local tostring = tostring
 
 local classes = {}
 
+local SYM_CONTENT = {}  ---@type Symbol
+
+---@class Comment
+
+local Comment_mt = {
+	__tostring = function (self)
+		return '<!--'..(self[SYM_CONTENT] or '')..'-->'
+	end,
+}
+---Comment.
+---
+---Defined at https://html.spec.whatwg.org/#comments
+---@param content string?
+---@return Comment
+function classes.Comment(content)
+	if content then
+		-- the text must not start with the string ">" or "->",
+		-- nor contain the strings "<!--", "-->", or "--!>",
+		-- nor end with the string "<!-"
+		local s = '--'..content..'-'
+		if
+			s:find('<!--', 1, true)
+			or s:find('-->', 1, true)
+			or content:find('--!>', 1, true)
+		then
+			error('invalid comment content: '..content, 2)
+		end
+	end
+	return setmt({[SYM_CONTENT] = content}, Comment_mt)
+end
+
 ---@class Doctype
 -- Specs:
 --   HTML:
@@ -20,18 +51,17 @@ classes.Doctype = {
 	}),
 }
 
-local SYM_RAW_CONTENT = {}  ---@type Symbol
 
 local Raw_mt
 Raw_mt = {  ---@type metatable
 	__tostring = function (self)
-		return self[SYM_RAW_CONTENT]
+		return self[SYM_CONTENT]
 	end,
 	__concat = function (left, right)
 		if getmt(left) ~= Raw_mt or getmt(right) ~= Raw_mt then
 			error('Raw object can only be concatenated with another Raw object', 2)
 		end
-		return setmt({[SYM_RAW_CONTENT] = left[SYM_RAW_CONTENT]..right[SYM_RAW_CONTENT]}, Raw_mt)
+		return setmt({[SYM_CONTENT] = left[SYM_CONTENT]..right[SYM_CONTENT]}, Raw_mt)
 	end,
 	__newindex = function ()
 		error('Raw object is not mutable', 2)
@@ -42,7 +72,7 @@ Raw_mt = {  ---@type metatable
 ---@param content any value to be converted to string by `tostring()`
 ---@return table
 function classes.Raw(content)
-	return setmt({[SYM_RAW_CONTENT] = tostring(content)}, Raw_mt)
+	return setmt({[SYM_CONTENT] = tostring(content)}, Raw_mt)
 end
 
 return classes
